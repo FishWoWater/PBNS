b2e2bc519a32fb7b491e018e349093e355122552
FishWoWater <dreamboy.gns@sjtu.edu.cn>
2022-04-25 20:36:16 +0800

    add resizer for arbitrary poses; add model

--- PBNS_Resizer/train_posedresizer_with_posedblendshapes.sh
+++ PBNS_Resizer/train_posedresizer_with_posedblendshapes.sh
@@ -1,1 +1,2 @@
-python train.py -g 0 -b pbns_posedsbs_female.pkl -o posed_sbs_female_outfit -n pbns_posedsbs_female -p 2
+set -x
+python train.py -g 0 -b pbns_posedsbs_male.pkl -o posed_sbs_male_outfit -n pbns_posedsbs_male -p 2
